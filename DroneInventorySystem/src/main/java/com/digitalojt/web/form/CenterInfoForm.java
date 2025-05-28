5a82d981977a5f7ae483618cb76a7e60af638c28
chenenmei <enmeichen66@gmail.com>
2025-05-28 09:26:49 +0900

    差分確認

---
+++ DroneInventorySystem/src/main/java/com/digitalojt/web/form/CenterInfoForm.java
@@ -0,0 +1,46 @@
+package com.digitalojt.web.form;
+
+import com.digitalojt.web.consts.ModelAttributeContents;
+import com.digitalojt.web.validation.CenterInfoFormValidator;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+/**
+ * 在庫センター情報画面のフォームクラス
+ * 
+ * @author dotlife
+ *
+ */
+@Data
+@CenterInfoFormValidator
+public class CenterInfoForm {
+
+	/**センター名*/
+    @Size(max = ModelAttributeContents.MAX_CENTER_NAME_LENGTH, message = "{centerName.length.wrongInput}")
+	private String centerName;
+
+	/**都道府県*/
+	private String region;
+
+	/**
+	 * 容量(From)
+	 */
+	private Integer storageCapacityFrom;
+
+	/**
+	 * 容量(To)
+	 */
+	private Integer storageCapacityTo;
+
+	/**
+	 * 容量(From)のデフォルト値を設定
+	 * @return
+	 */
+	@PostConstruct
+	public void init() {
+		// この処理は必要ない場合があります。空のメソッドにします。
+		// 自動初期化が問題を引き起こす可能性があります。
+	}
+}
